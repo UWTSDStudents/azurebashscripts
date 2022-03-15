c69efe6723f61db9e00599fd104efb9e3610c707
Mike Dacey <michael.dacey@uwtsd.ac.uk>
2022-03-15 19:36:05 +0000

    Fix 1

--- aksstudentnscreate.sh
+++ aksstudentnscreate.sh
@@ -1,8 +1,9 @@
 # Read all the users from the K8sStudents group
 KUSERS=$(az ad group member list -g K8sStudents  --query [].userPrincipalName -o tsv)
 
+delimiter=" "
 # Split the string into a bash array called users
-IFS=' ' read -r -a users <<< "$KUSERS"
+declare -a array=($(echo $KUSERS | tr "$delimiter" " "))
 
 # Iterate over the array
 for user in "${users[@]}"
