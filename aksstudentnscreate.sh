7d9fea04ea7ec8dfd18d6480b1b5398959d9a20a
Mike Dacey <michael.dacey@uwtsd.ac.uk>
2022-03-21 16:55:59 +0000

    Fixes to azstudentcreate

--- aksstudentnscreate.sh
+++ aksstudentnscreate.sh
@@ -10,6 +10,8 @@ declare -a users=($(echo $KUSERS| tr " " " "))
 # Get namespace names
 delimiter="_"
 # Split the string into a bash array called users
+# e.g. fred_student.uwtsd.ac.uk#EXT#@lee...cros
+# is split into two strings "fred" and "student.uwtsd.ac.uk#EXT#@lee...cros"
 declare -a users_sp=($(echo $KUSERS| tr "$delimiter" " "))
 users_ns=()
 
@@ -17,20 +19,37 @@ users_ns=()
 i=0
 for user in "${users_sp[@]}"
 do
+   # We split the string, so skip every second string
    if [ $(($i%2)) -eq 0 ]
    then
+		# Convert . to - before create namespace name
+		# e.g. fred.1234 becomes fred-1234-ns
 	    users_ns+=(${user//./-}-ns)
    fi
 
    i=$(($i+1))
 done
 
+# Get a list of all the namespace names
+ns=$(kubectl get ns -o name)
+declare -a ns=($(echo $ns| tr " " " "))
 
 i=0
 for user in "${users[@]}"
 do
-	echo "Creating namespace ${users_ns[$i]} assigned to ${user}"
-	#kubectl create namespace ${user}-ns
-	#az role assignment create --role "Azure Kubernetes Service RBAC Writer" --assignee $user --scope "$AKS_ID/namespaces/${users_ns[$i]}"
+	#nstest=$(kubectl describe namespace ${users_ns[$i]})
+	#if [ -z "$nstest" ]
+	if [[ ! ${ns[*]} =~ "namespace/${users_ns[$i]}" ]]
+	then 
+		echo "Creating namespace ${users_ns[$i]} assigned to ${user}"
+		kubectl create namespace ${users_ns[$i]}
+		az role assignment create --role "Azure Kubernetes Service RBAC Writer" --assignee $user --scope "$AKS_ID/namespaces/${users_ns[$i]}"
+	else 
+		# Find index of first # marking end of the username
+		s=`expr index $users[$i] "#*"`
+		s=$(($s-1))
+		echo "Not creating namespace for ${users[0]::s}. Namespace ${users_ns[$i]} already exists"; 
+	fi
+	
 	i=$(($i+1))
 done
